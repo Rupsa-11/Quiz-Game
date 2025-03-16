54d7abcaab529477bde95f179eb0c197495a2b55
Rupsa-11 <140781585+Rupsa-11@users.noreply.github.com>
2025-03-16 12:44:59 +0530

    Add files via upload

---
+++ script (2).js
@@ -0,0 +1,103 @@
+const questions = [
+  {
+    question: "What is the capital of France?",
+    answers: [
+      { text: "Paris", correct: true },
+      { text: "Berlin", correct: false },
+      { text: "Madrid", correct: false },
+      { text: "Florida", correct: false }
+    ]
+  },
+  {
+    question: "Which is the largest Desert in the World?",
+    answers: [
+      { text: "Kalahari", correct: false },
+      { text: "Sahara", correct: false },
+      { text: "Gobi", correct: false },
+      { text: "Antarctica", correct: true }
+    ]
+  }
+];
+
+const questionElement = document.getElementById("question");
+const answerElements = document.getElementById("answer-button"); 
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+  currentQuestionIndex = 0;
+  score = 0;
+  nextButton.style.display = "none";
+  showQuestions();
+}
+
+function showQuestions() {
+  resetState();
+  let currentQuestion = questions[currentQuestionIndex];
+  let questionNo = currentQuestionIndex + 1;
+  questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+  currentQuestion.answers.forEach(answer => {
+    const button = document.createElement("button");
+    button.innerHTML = answer.text;
+    button.classList.add("btn");
+    answerElements.appendChild(button);
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
+  });
+}
+
+function resetState() {
+  nextButton.style.display = "none";
+  while (answerElements.firstChild) {
+    answerElements.removeChild(answerElements.firstChild);
+  }
+}
+
+function selectAnswer(e) {
+  const selectedBtn = e.target;
+  const isCorrect = selectedBtn.dataset.correct === "true";
+  if (isCorrect) {
+    selectedBtn.classList.add("correct");
+    score++;
+  } else {
+    selectedBtn.classList.add("incorrect");
+  }
+  Array.from(answerElements.children).forEach(button => {
+    if (button.dataset.correct === "true") {
+      button.classList.add("correct");
+    }
+    button.disabled = true;
+  });
+  nextButton.style.display = "block";
+}
+
+function showScore() {
+  resetState();
+  questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+  nextButton.innerHTML = "Play Again";
+  nextButton.style.display = "block";
+}
+
+function handleNextButton() {
+  currentQuestionIndex++;
+  if (currentQuestionIndex < questions.length) {
+    showQuestions();
+  } else {
+    showScore();
+  }
+}
+
+nextButton.addEventListener("click", () => {
+  if (currentQuestionIndex < questions.length) {
+    handleNextButton();
+  } else {
+    startQuiz();
+  }
+});
+
+startQuiz();
